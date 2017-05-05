2673fd703b68f0feab0de16652dc9de939360750
Patrik Hartwig <pat.ggf@gmail.com>
2017-05-05 21:35:23 +1000

    accept OPTIONS...

--- snub-http.js
+++ snub-http.js
@@ -15,7 +15,20 @@ module.exports = function (config) {
 
   return function (snub) {
     const requestHandler = (request, response) => {
+
+      if (request.method === 'OPTIONS') {
+        var headers = {};
+        headers["Access-Control-Allow-Origin"] = "*";
+        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+        headers["Access-Control-Allow-Credentials"] = false;
+        headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+        headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+        response.writeHead(200, headers);
+        return response.end();
+      }
+
       var urlParsed = url.parse(request.url);
+
       var reqObj = {
         method: request.method,
         path: urlParsed.pathname,
